8e61af6b939a82a4f0c1cc1ef88b69b2b468a7db
comstering <coms1101@naver.com>
2021-11-24 04:45:28 +0900

    ✨ Add: Create Socket Class
    Socket
    TcpSocket: public Socket
    UdpSocket: public Socket

---
+++ src/socket.h
@@ -0,0 +1,38 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#endif
+
+#ifdef __linux__
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#endif
+
+#include "spdlog/spdlog.h"
+
+#define BUFSIZE 65536
+
+class Socket
+{
+public:
+    int sock_{0};
+    struct sockaddr_in sockAddr_;
+
+public:
+    Socket() {}
+    virtual ~Socket() {}
+
+    int disconnect();
+    virtual int send(char* buf, size_t len) = 0;
+    virtual int recv(char* buf, size_t len) = 0;
+};
+
+#endif // SOCKET_H
